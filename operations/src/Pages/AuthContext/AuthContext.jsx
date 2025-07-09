db8ddda00f1948aab4d5c04e7824bdd42cf66c98
kanishka1302 <134297218+kanishka1302@users.noreply.github.com>
2025-07-09 17:38:54 +0530

    Update AuthContext.jsx

--- operations/src/Pages/AuthContext/AuthContext.jsx
+++ operations/src/Pages/AuthContext/AuthContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 
+
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(() => {
     // Initialize state from localStorage
